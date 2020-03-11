0da08bdf41c322d5d49a139c9ecebe8a834113dd
jeff_simon <jeffsbillinfo@gmail.com>
2020-03-10 22:03:01 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @name = name     
+        @roster = {}   
+    end
+
+    def add_student(student_name, grade)
+        #binding.pry()
+#        @roster[student_name] = grade
+        if(@roster[grade] == nil)
+            @roster[grade] = []
+        end        
+        @roster[grade].push(student_name)
+        #binding.pry()
+    end
+
+    def grade(gradeToRetrieve)
+       @roster[gradeToRetrieve] 
+    end
+
+    def sort
+        @roster.each{|key,value|
+            @roster[key] = value.sort
+    }
+    end
+end
+
+
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+puts school.roster
